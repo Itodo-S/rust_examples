55ba77e52c2021ccb269430f7fc305c382341a6d
Itodo-S <itodosimonitodo1@gmail.com>
2025-10-28 22:30:13 +0100

    initialized the rust project using cargo

---
+++ learning_rust/target/debug/deps/learning_rust-ea1b9d8daa65208f.d
@@ -0,0 +1,5 @@
+/home/sayae/Desktop/SHITTY_CODES/RUST/rust_by_example/learning_rust/target/debug/deps/learning_rust-ea1b9d8daa65208f: src/main.rs
+
+/home/sayae/Desktop/SHITTY_CODES/RUST/rust_by_example/learning_rust/target/debug/deps/learning_rust-ea1b9d8daa65208f.d: src/main.rs
+
+src/main.rs:
---
+++ learning_rust/target/debug/learning_rust.d
@@ -0,0 +1,1 @@
+/home/sayae/Desktop/SHITTY_CODES/RUST/rust_by_example/learning_rust/target/debug/learning_rust: /home/sayae/Desktop/SHITTY_CODES/RUST/rust_by_example/learning_rust/src/main.rs
